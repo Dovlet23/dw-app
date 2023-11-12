a705e4b884b61cb1cda135f28536856aaf1b8307
Dovlet <dovlrt2398@icloud.com>
2023-11-12 22:39:06 +0300

    first commit

---
+++ app/Http/Controllers/HomeControllers.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class HomeControllers extends Controller
+{
+    public function test()
+    {
+        echo 'Hello';
+    }
+}
+public function createArticle()
